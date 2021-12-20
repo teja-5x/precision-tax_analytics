0cde6dd0892e9ad60aa52aa449c868a1624f8739
Qi Wu <qi+precisiontax@5x.co>
2021-12-20 15:24:51 +0000

    employee_subtas ks_permonth

---
+++ models/employee_performance/employee_subtasks_permonth.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+select 
+    E.ID,
+    E.FirstName,
+    Count(CT.ContactTasks_SubTasksId) as SubTasksCountPerEmployee
+from Fivetran_db.SQL_Server_DBO.Employees E  
+JOIN Fivetran_db.SQL_Server_DBO.ContactTasks_SubTasks CT 
+ON CT.AssignedToEmployeeID = E.ID
+where 
+    E.InActive = 0 
+    and YEAR(CT.InsertedDate)  = YEAR(CURRENT_TIMESTAMP)
+    and MONTH(CT.InsertedDate) = MONTH(CURRENT_TIMESTAMP)
+group by E.ID, E.FirstName
+order by 3 desc;
