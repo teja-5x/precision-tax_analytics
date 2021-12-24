412050e065ea3c6f85be3a5b2db1e827ae66d396
Qi Wu <qi+precisiontax@5x.co>
2021-12-24 06:27:52 +0000

    Total_Number_Of_Resolution_Clients

--- models/employee_performance/TotalNumberOfResolutionClients.sql
+++ models/employee_performance/TotalNumberOfResolutionClients.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='view') }}
 
-select count(distinct(ContactID))
+select count(distinct(ContactID)) as Total_Number_Of_Resolution_Clients
 from {{ ref('Current_Year_Resolution_Contracts') }}
 where 
 
