5771bc5d114debff44bb246eb270bcf114f38170
Qi Wu <qi+precisiontax@5x.co>
2021-12-24 08:27:43 +0000

    BillingContracts

--- models/employee_performance/AllBillingContracts.sql
+++ models/employee_performance/AllBillingContracts.sql
@@ -1,6 +1,6 @@
 {{ config(materialized='table') }}
 
-with AllBillingContracts as 
+with BillingContracts as 
 (
     select 
         *
@@ -9,4 +9,4 @@ with AllBillingContracts as
     --where TPO = 0 
     --and billingEndDate >= CONCAT(year(current_timestamp),'-','01','-','01')
 )
-select * from AllBillingContracts
+select * from BillingContracts
